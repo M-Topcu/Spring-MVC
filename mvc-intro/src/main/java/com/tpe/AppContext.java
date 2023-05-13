4a0fd0f0861404012cc8880968d60e8f17f72cb1
muhammet topcu <mamitopcu35@gmail.com>
2023-05-13 21:27:11 +0200

    mvc2

---
+++ mvc-intro/src/main/java/com/tpe/AppContext.java
@@ -0,0 +1,51 @@
+package com.tpe;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+@Configuration
+@PropertySource("classpath:db.properties")
+public class AppContext {
+
+	@Autowired
+	private Environment enviroment;
+	
+	@Bean
+	public LocalSessionFactoryBean sessionFactory() {
+		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+		sessionFactory.setDataSource(dataSource());
+		sessionFactory.setPackagesToScan(new String[] {"com.tpe.domain"});
+		sessionFactory.setHibernateProperties(hibernateProperties());
+		return sessionFactory;
+	}
+	
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(enviroment.getRequiredProperty("db.driverClassName"));
+		dataSource.setUrl(enviroment.getRequiredProperty("db.url"));
+		dataSource.setUsername(enviroment.getRequiredProperty("db.username"));
+		dataSource.setPassword(enviroment.getRequiredProperty("db.password"));
+		
+		return dataSource;
+	}
+	
+	private Properties hibernateProperties() {
+		Properties properties = new Properties();
+		properties.put("hibernate.dialect", enviroment.getRequiredProperty("hibernate.dialect"));
+		properties.put("hibernate.show_sql", enviroment.getRequiredProperty("hibernate.show_sql"));
+		properties.put("hibernate.format_sql", enviroment.getRequiredProperty("hibernate.format_sql"));
+		properties.put("hibernate.hbm2ddl.auto", enviroment.getRequiredProperty("hibernate.hbm2ddl.auto"));
+		return properties;
+	}
+	
+}
---
+++ mvc-intro/src/main/java/com/tpe/WebMvcConfig.java
@@ -0,0 +1,32 @@
+package com.tpe;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.tpe")
+public class WebMvcConfig implements WebMvcConfigurer{
+
+	// viewler icin
+	@Bean
+	public InternalResourceViewResolver resolver(){
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setViewClass(JstlView.class);
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
+
+	//js, css, image dosyalarini koymak icin
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+}
